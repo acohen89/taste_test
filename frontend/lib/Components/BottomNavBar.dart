8fa2315435bfc5ed02fcc783ce80aa4c91dfc996
acohen89 <acohen89@binghamton.edu>
2024-06-11 09:21:40 -0700

    impoved bottom nav bar

--- frontend/lib/Components/BottomNavBar.dart
+++ frontend/lib/Components/BottomNavBar.dart
@@ -1,14 +1,10 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:taste_test/Shared/constants.dart';
 
-
 class BottomNavBar extends StatefulWidget {
   final int startIndex;
-  
-   const BottomNavBar({
+  const BottomNavBar({
     required this.startIndex,
     super.key,
   });
@@ -20,54 +16,65 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    const containerPadding = 4.0; 
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    final double hoPadding = width/30.7; 
+    final double bottomPadding = height/38.83; 
+    final double topPadding = height/ 66.57; 
     const iconPadding = EdgeInsets.only(top: 2, left: 4, right: 4, bottom: 2);
     return Container(
       color: Colors.white,
-      child:  Container(
-        padding: const EdgeInsets.only(bottom:containerPadding, left: containerPadding, right: containerPadding, top:containerPadding),
-        child: GNav(
-          selectedIndex: widget.startIndex,
-          onTabChange: (value) {
-            switch (value) {
-              case 0: // In Progress Recipes
-                Navigator.pushNamed(context, "inProgressRecipes");
-              case 1: // Create
-                Navigator.pushNamed(context, "createRecipe");
-              case 2: // Finished recipes
-                Navigator.pushNamed(context, "home");
-              case 3: // Settings
-                Navigator.pushNamed(context, "settings");
-                
-            }
-          },
-         backgroundColor: Colors.white,
-         color: greyColor, 
-         activeColor: Colors.white,
-         tabBackgroundColor: greyColor,
-         gap: 4, 
-         padding: iconPadding, 
-          tabs: const [
-            GButton(
-              icon: Icons.edit,
-              text: 'Recipes In Progress',
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 4,
             ),
-            GButton(
-              icon: Icons.done,
-              text: 'Finished Recipes',
-            ),
-            GButton(
-              icon: Icons.create,
-              text: 'Create',
-            ),
-            GButton(
-              icon: Icons.settings,
-              text: 'Settings',
-            )
-          ]
+          ],
         ),
+        padding: EdgeInsets.only(bottom: bottomPadding, top: topPadding, right: hoPadding, left: hoPadding),
+        child: GNav(
+            selectedIndex: widget.startIndex,
+            onTabChange: (value) {
+              switch (value) {
+                case 0: // In Progress Recipes
+                  Navigator.pushNamed(context, "inProgressRecipes");
+                case 1: // Create
+                  Navigator.pushNamed(context, "createRecipe");
+                case 2: // Finished recipes
+                  Navigator.pushNamed(context, "home");
+                case 3: // Settings
+                  Navigator.pushNamed(context, "settings");
+              }
+            },
+            backgroundColor: Colors.white,
+            color: greyColor,
+            activeColor: Colors.white,
+            tabBackgroundColor: greyColor,
+            gap: 4,
+            padding: iconPadding,
+            tabs: const [
+              GButton(
+                icon: Icons.edit,
+                text: 'Recipes In Progress',
+              ),
+              GButton(
+                icon: Icons.done,
+                text: 'Finished Recipes',
+              ),
+              GButton(
+                icon: Icons.create,
+                text: 'Create',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Settings',
+              )
+            ]),
       ),
     );
   }
 }
-
